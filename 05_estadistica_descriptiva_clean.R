7f8e2b9804bc9b0626e133ddb7c12b2897637b94
fgonzalezaon <fgonzalezaon@gmail.com>
2025-12-04 11:19:42 -0300

    Add files via upload

---
+++ 05_estadistica_descriptiva_clean.R
@@ -0,0 +1,10 @@
+#Evaluacion impacto limpieza
+
+#Las estadísticas descriptivas reportadas incluyen media, moda. mediana, desvío estándar, 
+#valores mínimo y máximo, y los percentiles 25 y 75.
+
+estadisticas(gini_interpolado)
+estadisticas(pobreza_winsor)
+estadisticas(ingreso_low_20_interpolado)
+estadisticas(pbi_log)
+estadisticas(desempleo_interpolado)
